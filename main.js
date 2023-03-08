6185ecfdc08ba755e20d4a2d0fcdf29fa7e02c34
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-03-08 07:37:43 -0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const imgAprovado = '<img src="imagens/aprovado.png" alt="Aprovado" />';
 const imgReprovado = '<img src="imagens/reprovado.png" alt="Reprovado" />';
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
 let linhas = "";
 
@@ -17,18 +19,22 @@ function adicionaLinha() {
   const inputNomeAtividade = document.getElementById("nome-atividade");
   const inputNotaAtividade = document.getElementById("nota-atividade");
 
-  atividades.push(inputNomeAtividade.value);
-  notas.push(parseFloat(inputNotaAtividade.value));
+  if (atividades.includes(inputNomeAtividade.value)) {
+    alert("Atividade já cadastrada");
+  } else {
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
-  let linha = `<tr>`;
-  linha += `<td>${inputNomeAtividade.value}</td>`;
-  linha += `<td>${inputNotaAtividade.value}</td>`;
-  linha += `<td>${
-    inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado
-  } </td>`;
-  linha += `</tr>`;
+    let linha = `<tr>`;
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${
+      inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado
+    } </td>`;
+    linha += `</tr>`;
 
-  linhas += linha;
+    linhas += linha;
+  }
 
   inputNomeAtividade.value = "";
   inputNotaAtividade.value = "";
@@ -41,7 +47,7 @@ function atualizaMediaFinal() {
   const mediaFinal = somaDasNotas();
   document.getElementById("media-final-valor").innerHTML = mediaFinal;
   document.getElementById("media-final-resultado").innerHTML =
-    mediaFinal >= 7 ? imgAprovado : imgReprovado;
+    mediaFinal >= 7 ? spanAprovado : spanReprovado;
 }
 
 function somaDasNotas() {
