0233a31013621a5c04f2a334acc67f7c06b76b71
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2023-03-08 07:42:28 -0800

    update

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function atualizaTabela() {
 }
 function atualizaMediaFinal() {
   const mediaFinal = somaDasNotas();
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(2));
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= 7 ? spanAprovado : spanReprovado;
 }
